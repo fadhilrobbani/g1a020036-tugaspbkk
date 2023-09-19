edf46deae8db067baf82624e910dc02e0945bee5
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-09-19 20:45:09 +0700

    create all table migration or table

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Category::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $categoriesData = ['journal','novel','encyclopedia','technologies'];
+
+        foreach ($categoriesData as $data) {
+            Category::insert([
+                'name' => $data,
+            ]);
+        }
+    }
+}
