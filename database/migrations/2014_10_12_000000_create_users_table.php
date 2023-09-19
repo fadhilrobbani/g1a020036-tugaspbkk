38b6006a556652811a6224f4488d21dccf31f9b2
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-09-19 19:26:25 +0700

    prepare database for user and roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('fullname');
+            $table->string('phone')->nullable();
+            $table->string('status')->default('inactive');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
